d18e68920ba27500660cf299f2c92c1c636f7458
lilesh.bhade <lilesh.bhade@azlo.com>
2021-04-02 16:11:05 -0700

    Figure number insertion in middle

--- src/main/java/interview/Figure.java
+++ src/main/java/interview/Figure.java
@@ -1,17 +1,29 @@
 package interview;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * {1,2.3} add 2 -> {1,2,3,4}
  * {1,2,3} add 5 -> {1,2,3,4}
+ *
+ *
  */
 public class Figure {
     public static void main(String[] args) {
         int[] array = {1, 2, 3};
-        int k = 2;
+        int k = 5;
 
+        if(k >array.length){
+            int newLength  = array.length+1;
+            int[]  newArray =  new int[newLength];
+            System.arraycopy(array,0, newArray,0, array.length);
+            System.out.println(newLength);
+            newArray[newLength-1] =  newLength;
+            System.out.println(Arrays.toString(newArray));;
+            return;
+        }
         List<Integer> newList = new ArrayList<>();
         boolean flag = false;
         int index = 0;
